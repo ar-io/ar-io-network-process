ee70818fb8578610666430d2da778a3d082a0fa7
dtfiedler <dylan@ar.io>
2024-06-26 21:05:19 -0600

    chore(test): add gar integration tests

--- src/main.lua
+++ src/main.lua
@@ -744,7 +744,7 @@ Handlers.add(
 			delegateRewardShareRatio = tonumber(msg.Tags["Delegate-Reward-Share-Ratio"])
 				or gateway.settings.delegateRewardShareRatio,
 			properties = msg.Tags.Properties or gateway.settings.properties,
-			autoStake = msg.Tags["Auto-Stake"] == "true" or gateway.settings.autoStake,
+			autoStake = not msg.Tags["Auto-Stake"] and gateway.settings.autoStake or msg.Tags["Auto-Stake"] == "true",
 		}
 		local observerAddress = msg.Tags["Observer-Address"] or gateway.observerAddress
 		local status, result =
--- test/gar.test.js
+++ test/gar.test.js
@@ -22,26 +22,26 @@ describe('GatewayRegistry', async () => {
     );
   }
 
-  describe('Join-Network', () => {
-    const validGatewayTags = [
-      { name: 'Action', value: 'Join-Network' },
-      { name: 'Label', value: 'test-gateway' },
-      { name: 'Note', value: 'test-note' },
-      { name: 'FQDN', value: 'test-fqdn' },
-      { name: 'Operator-Stake', value: '500000000000' }, // 50K IO
-      { name: 'Port', value: '443' },
-      { name: 'Protocol', value: 'https' },
-      { name: 'Allow-Delegated-Staking', value: 'true' },
-      { name: 'Min-Delegated-Stake', value: '500000000' }, // 500 IO
-      { name: 'Delegate-Reward-Share-Ratio', value: '0' },
-      { name: 'Observer-Address', value: STUB_ADDRESS },
-      {
-        name: 'Properties',
-        value: 'FH1aVetOoulPGqgYukj0VE0wIhDy90WiQoV3U2PeY44',
-      },
-      { name: 'Auto-Stake', value: 'true' },
-    ];
+  const validGatewayTags = [
+    { name: 'Action', value: 'Join-Network' },
+    { name: 'Label', value: 'test-gateway' },
+    { name: 'Note', value: 'test-note' },
+    { name: 'FQDN', value: 'test-fqdn' },
+    { name: 'Operator-Stake', value: '50000000000' }, // 50K IO
+    { name: 'Port', value: '443' },
+    { name: 'Protocol', value: 'https' },
+    { name: 'Allow-Delegated-Staking', value: 'true' },
+    { name: 'Min-Delegated-Stake', value: '500000000' }, // 500 IO
+    { name: 'Delegate-Reward-Share-Ratio', value: '0' },
+    { name: 'Observer-Address', value: STUB_ADDRESS },
+    {
+      name: 'Properties',
+      value: 'FH1aVetOoulPGqgYukj0VE0wIhDy90WiQoV3U2PeY44',
+    },
+    { name: 'Auto-Stake', value: 'true' },
+  ];
 
+  describe('Join-Network', () => {
     it('should allow joining of the network record', async () => {
       const joinNetworkResult = await handle({
         Tags: validGatewayTags,
@@ -61,7 +61,7 @@ describe('GatewayRegistry', async () => {
       const gatewayData = JSON.parse(gateway.Messages[0].Data);
       assert.deepEqual(gatewayData, {
         observerAddress: STUB_ADDRESS,
-        operatorStake: 500_000_000_000,
+        operatorStake: 50_000_000_000,
         totalDelegatedStake: 0,
         status: 'joined',
         delegates: [],
@@ -138,5 +138,295 @@ describe('GatewayRegistry', async () => {
         assert.equal(gatewayData, null);
       });
     }
+  });
+
+  describe('Update-Gateway-Settings', () => {
+    it('should allow updating the gateway settings', async () => {
+      const joinNetworkResult = await handle({
+        Tags: validGatewayTags,
+      });
+
+      const joinNetworkData = JSON.parse(joinNetworkResult.Messages[0].Data);
+
+      const updateGatewaySettingsResult = await handle(
+        {
+          Tags: [
+            { name: 'Action', value: 'Update-Gateway-Settings' },
+            { name: 'Label', value: 'new-label' },
+            { name: 'Note', value: 'new-note' },
+            { name: 'FQDN', value: 'new-fqdn' },
+            { name: 'Port', value: '80' },
+            { name: 'Protocol', value: 'https' },
+            { name: 'Allow-Delegated-Staking', value: 'false' },
+            { name: 'Min-Delegated-Stake', value: '1000000000' }, // 1K IO
+            { name: 'Delegate-Reward-Share-Ratio', value: '10' },
+            {
+              name: 'Properties',
+              value: 'FH1aVetOoulPGqgYukj0VE0wIhDy90WiQoV3U2PeY44',
+            },
+            { name: 'Auto-Stake', value: 'false' },
+          ],
+        },
+        joinNetworkResult.Memory,
+      );
+
+      // check the gateway record from contract
+      const gateway = await handle(
+        {
+          Tags: [
+            { name: 'Action', value: 'Gateway' },
+            { name: 'Address', value: STUB_ADDRESS },
+          ],
+        },
+        updateGatewaySettingsResult.Memory,
+      );
+      const gatewayData = JSON.parse(gateway.Messages[0].Data);
+
+      assert.deepEqual(gatewayData, {
+        observerAddress: STUB_ADDRESS,
+        operatorStake: 50_000_000_000,
+        totalDelegatedStake: 0,
+        status: 'joined',
+        delegates: [],
+        vaults: [],
+        startTimestamp: joinNetworkData.startTimestamp,
+        settings: {
+          label: 'new-label',
+          note: 'new-note',
+          fqdn: 'new-fqdn',
+          port: 80,
+          protocol: 'https',
+          autoStake: false,
+          allowDelegatedStaking: false,
+          minDelegatedStake: 1_000_000_000,
+          delegateRewardShareRatio: 10,
+          properties: 'FH1aVetOoulPGqgYukj0VE0wIhDy90WiQoV3U2PeY44',
+        },
+        stats: {
+          passedConsecutiveEpochs: 0,
+          failedConsecutiveEpochs: 0,
+          totalEpochCount: 0,
+          failedEpochCount: 0,
+          passedEpochCount: 0,
+          prescribedEpochCount: 0,
+          observedEpochCount: 0,
+        },
+      });
+    });
+  });
+
+  describe('Increase-Operator-Stake', () => {
+    // join the network and then increase stake
+    it('should allow increasing operator stake', async () => {
+      const joinNetworkResult = await handle({
+        Tags: validGatewayTags,
+      });
+
+      const joinNetworkData = JSON.parse(joinNetworkResult.Messages[0].Data);
+
+      const increaseStakeResult = await handle(
+        {
+          Tags: [
+            { name: 'Action', value: 'Increase-Operator-Stake' },
+            { name: 'Quantity', value: '10000000000' }, // 10K IO
+          ],
+        },
+        joinNetworkResult.Memory,
+      );
+
+      // check the gateway record from contract
+      const gateway = await handle(
+        {
+          Tags: [
+            { name: 'Action', value: 'Gateway' },
+            { name: 'Address', value: STUB_ADDRESS },
+          ],
+        },
+        increaseStakeResult.Memory,
+      );
+      const gatewayData = JSON.parse(gateway.Messages[0].Data);
+      assert.deepEqual(gatewayData, {
+        observerAddress: STUB_ADDRESS,
+        operatorStake: 60_000_000_000,
+        totalDelegatedStake: 0,
+        status: 'joined',
+        delegates: [],
+        vaults: [],
+        startTimestamp: joinNetworkData.startTimestamp,
+        settings: {
+          label: 'test-gateway',
+          note: 'test-note',
+          fqdn: 'test-fqdn',
+          port: 443,
+          protocol: 'https',
+          allowDelegatedStaking: true,
+          minDelegatedStake: 500_000_000,
+          delegateRewardShareRatio: 0,
+          properties: 'FH1aVetOoulPGqgYukj0VE0wIhDy90WiQoV3U2PeY44',
+          autoStake: true,
+        },
+        stats: {
+          passedConsecutiveEpochs: 0,
+          failedConsecutiveEpochs: 0,
+          totalEpochCount: 0,
+          failedEpochCount: 0,
+          passedEpochCount: 0,
+          prescribedEpochCount: 0,
+          observedEpochCount: 0,
+        },
+      });
+    });
+  });
+
+  describe('Decrease-Operator-Stake', () => {
+    // join the network and then increase stake
+    it('should allow decrease the operator stake', async () => {
+      // filter the operator-stake tag
+      const overrideTags = validGatewayTags.filter(
+        (tag) => tag.name !== 'Operator-Stake',
+      );
+
+      const joinNetworkResult = await handle({
+        Tags: [
+          ...overrideTags,
+          { name: 'Operator-Stake', value: '60000000000' }, // 60K IO
+        ],
+      });
+
+      const joinNetworkData = JSON.parse(joinNetworkResult.Messages[0].Data);
+
+      const decreaseStakeResult = await handle(
+        {
+          Tags: [
+            { name: 'Action', value: 'Decrease-Operator-Stake' },
+            { name: 'Quantity', value: '10000000000' }, // 10K IO
+          ],
+        },
+        joinNetworkResult.Memory,
+      );
+
+      const decreaseResult = JSON.parse(decreaseStakeResult.Messages[0].Data);
+
+      // check the gateway record from contract
+      const gateway = await handle(
+        {
+          Tags: [
+            { name: 'Action', value: 'Gateway' },
+            { name: 'Address', value: STUB_ADDRESS },
+          ],
+        },
+        decreaseStakeResult.Memory,
+      );
+      const gatewayData = JSON.parse(gateway.Messages[0].Data);
+      assert.deepEqual(gatewayData, {
+        observerAddress: STUB_ADDRESS,
+        operatorStake: 50_000_000_000,
+        totalDelegatedStake: 0,
+        status: 'joined',
+        delegates: [],
+        vaults: {
+          [STUB_ADDRESS]: {
+            balance: 10_000_000_000,
+            startTimestamp: decreaseResult.vaults[STUB_ADDRESS].startTimestamp,
+            endTimestamp:
+              decreaseResult.vaults[STUB_ADDRESS].startTimestamp +
+              1000 * 60 * 60 * 24 * 30, // thirty days
+          },
+        },
+        startTimestamp: joinNetworkData.startTimestamp,
+        settings: {
+          label: 'test-gateway',
+          note: 'test-note',
+          fqdn: 'test-fqdn',
+          port: 443,
+          protocol: 'https',
+          allowDelegatedStaking: true,
+          minDelegatedStake: 500_000_000,
+          delegateRewardShareRatio: 0,
+          properties: 'FH1aVetOoulPGqgYukj0VE0wIhDy90WiQoV3U2PeY44',
+          autoStake: true,
+        },
+        stats: {
+          passedConsecutiveEpochs: 0,
+          failedConsecutiveEpochs: 0,
+          totalEpochCount: 0,
+          failedEpochCount: 0,
+          passedEpochCount: 0,
+          prescribedEpochCount: 0,
+          observedEpochCount: 0,
+        },
+      });
+    });
+  });
+
+  // leave network
+  describe('Leave-Network', () => {
+    it('should allow leaving the network', async () => {
+      const joinNetworkResult = await handle({
+        Tags: validGatewayTags,
+      });
+
+      const joinNetworkData = JSON.parse(joinNetworkResult.Messages[0].Data);
+
+      const leaveNetworkResult = await handle(
+        {
+          Tags: [{ name: 'Action', value: 'Leave-Network' }],
+        },
+        joinNetworkResult.Memory,
+      );
+
+      const leaveNetworkData = JSON.parse(leaveNetworkResult.Messages[0].Data);
+
+      // check the gateway record from contract
+      const gateway = await handle(
+        {
+          Tags: [
+            { name: 'Action', value: 'Gateway' },
+            { name: 'Address', value: STUB_ADDRESS },
+          ],
+        },
+        leaveNetworkResult.Memory,
+      );
+      const gatewayData = JSON.parse(gateway.Messages[0].Data);
+      assert.deepEqual(gatewayData, {
+        observerAddress: STUB_ADDRESS,
+        operatorStake: 0,
+        totalDelegatedStake: 0,
+        status: 'leaving',
+        delegates: [],
+        endTimestamp:
+          leaveNetworkData.startTimestamp + 1000 * 60 * 60 * 24 * 90, // 90 days
+        vaults: {
+          [STUB_ADDRESS]: {
+            balance: 50_000_000_000,
+            startTimestamp: leaveNetworkData.startTimestamp,
+            endTimestamp:
+              leaveNetworkData.startTimestamp + 1000 * 60 * 60 * 24 * 90, // 90 days
+          },
+        },
+        startTimestamp: joinNetworkData.startTimestamp,
+        settings: {
+          label: 'test-gateway',
+          note: 'test-note',
+          fqdn: 'test-fqdn',
+          port: 443,
+          protocol: 'https',
+          allowDelegatedStaking: true,
+          minDelegatedStake: 500_000_000,
+          delegateRewardShareRatio: 0,
+          properties: 'FH1aVetOoulPGqgYukj0VE0wIhDy90WiQoV3U2PeY44',
+          autoStake: true,
+        },
+        stats: {
+          passedConsecutiveEpochs: 0,
+          failedConsecutiveEpochs: 0,
+          totalEpochCount: 0,
+          failedEpochCount: 0,
+          passedEpochCount: 0,
+          prescribedEpochCount: 0,
+          observedEpochCount: 0,
+        },
+      });
+    });
   });
 });
