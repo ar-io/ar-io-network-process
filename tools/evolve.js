1fe532b20bb183cb3566b36d2880572302602666
atticusofsparta <atticusofsparta@protonmail.com>
2024-08-02 15:06:08 -0600

    chore(ci/cd): add evolve ci

---
+++ tools/evolve.js
@@ -0,0 +1,15 @@
+import { AOProcess, createAoSigner } from '@ar.io/sdk';
+import { BUNDLED_AOS_LUA } from './constants';
+
+const registryId = process.env.REGISTRY_ID;
+const wallet = JSON.parse(process.env.WALLET);
+const process = new AOProcess({ processId: registryId });
+
+const signer = createAoSigner(new ArweaveSigner(wallet));
+
+const evolveResult = await process.send({
+  tags: [{ name: 'Action', value: 'Eval' }],
+  data: BUNDLED_AOS_LUA,
+  signer,
+});
+console.log(`Evolve result: ${evolveResult}`);
