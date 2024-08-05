1a238d4339ddab4f511dc6cf879d9214405ebbd9
dtfiedler <dylan@ar.io>
2024-08-05 09:51:28 -0600

    chore(ci): fix evolve script

--- tools/evolve.mjs
+++ tools/evolve.mjs
@@ -1,15 +1,14 @@
-import { AOProcess, createAoSigner } from '@ar.io/sdk';
-import { BUNDLED_AOS_LUA } from './constants.js';
+import { AOProcess, createAoSigner } from '@ar.io/sdk/node';
+import constants from './constants.js';
 
 const registryId = process.env.REGISTRY_ID;
 const wallet = JSON.parse(process.env.WALLET);
-const process = new AOProcess({ processId: registryId });
-
 const signer = createAoSigner(new ArweaveSigner(wallet));
+const networkProcess = new AOProcess({ processId: registryId });
 
-const evolveResult = await process.send({
+const evolveResult = await networkProcess.send({
   tags: [{ name: 'Action', value: 'Eval' }],
-  data: BUNDLED_AOS_LUA,
+  data: constants.BUNDLED_AOS_LUA,
   signer,
 });
 console.log(`Evolve result: ${evolveResult}`);
