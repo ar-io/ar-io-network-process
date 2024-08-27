63657ee4043d6ff609a1d43da7899d6fb63937af
dtfiedler <dylan@ar.io>
2024-08-26 18:25:55 -0600

    chore(test): setup github workflow to monitor process state

--- tools/bundle-aos.mjs
+++ tools/bundle-aos.mjs
@@ -1,6 +1,9 @@
-const path = require('path');
-const fs = require('fs');
-const { bundle } = require('./lua-bundler.js');
+import { bundle } from './lua-bundler.mjs';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function main() {
   console.log('Bundling Lua...');
