03b35a360dd2a1f14c70852307506e81f1ff4837
dtfiedler <dylan@ar.io>
2024-09-11 21:57:47 -0600

    chore(evolve): use AO_CU_URL in evolve script

--- tools/evolve.mjs
+++ tools/evolve.mjs
@@ -1,10 +1,15 @@
 import { AOProcess, createAoSigner, ArweaveSigner } from '@ar.io/sdk/node';
 import * as constants from './constants.mjs';
+import { connect } from '@permaweb/aoconnect';
 
-const processId = process.env.IO_NETWORK_PROCESS_ID;
 const wallet = JSON.parse(process.env.WALLET);
 const signer = createAoSigner(new ArweaveSigner(wallet));
-const networkProcess = new AOProcess({ processId });
+const networkProcess = new AOProcess({
+  processId: process.env.IO_NETWORK_PROCESS_ID,
+  ao: connect({
+    CU_URL: process.env.AO_CU_URL,
+  }),
+});
 
 const { id } = await networkProcess.send({
   tags: [{ name: 'Action', value: 'Eval' }],
