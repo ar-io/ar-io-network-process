058e2fc3cac67a3df04e9589b55b2d4c5e8a5bf6
dtfiedler <dylan@ar.io>
2024-11-25 07:43:27 -0600

    fix(gateways): remove delegates and vaults from gateways
    This will minimize the gateway request. The SDK and downstream apps are already using the newest APIs

--- src/gar.lua
+++ src/gar.lua
@@ -1027,7 +1027,9 @@ function gar.getPaginatedGateways(cursor, limit, sortBy, sortOrder)
 	for address, record in pairs(gateways) do
 		--- @diagnostic disable-next-line: inject-field
 		record.gatewayAddress = address
-		-- TODO: remove delegates here to avoid sending an unbounded array; to fetch delegates, use getPaginatedDelegates
+		-- remove delegates and vaults to avoid sending unbounded arrays, they can be fetched via getPaginatedDelegates and getPaginatedVaults
+		record.delegates = nil
+		record.vaults = nil
 		table.insert(gatewaysArray, record)
 	end
 
