dc0c75fb356c565c8abad7c3a147d8d5371ebf16
dtfiedler <dylan@ar.io>
2024-11-28 10:12:47 -0600

    chore(test): fix test

--- spec/primary_names_spec.lua
+++ spec/primary_names_spec.lua
@@ -94,7 +94,7 @@ describe("Primary Names", function()
 			"should create a primary name request and transfer the cost from the initiator to the protocol balance",
 			function()
 				_G.Balances = {
-					["user-requesting-primary-name"] = 100000000,
+					["user-requesting-primary-name"] = 10000000,
 				}
 				_G.NameRegistry.records = {
 					["test"] = {
@@ -122,11 +122,11 @@ describe("Primary Names", function()
 					},
 					fundingResult = {
 						newWithdrawVaults = {},
-						totalFunded = 100000000,
+						totalFunded = 10000000,
 					},
 				}, primaryNameRequest)
 				assert.are.equal(0, _G.Balances["user-requesting-primary-name"])
-				assert.are.equal(100000000, _G.Balances[ao.id])
+				assert.are.equal(10000000, _G.Balances[ao.id])
 			end
 		)
 	end)
