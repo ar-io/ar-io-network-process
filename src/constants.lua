73c41bd616ebf98be493ea5752e31b7d8c5dff5e
Derek Sonnenberg <derek@ardrive.io>
2024-12-06 12:02:23 -0600

    refactor: share defaultEpochLength constant PE-7208

--- src/constants.lua
+++ src/constants.lua
@@ -9,6 +9,9 @@ constants.oneWeekMs = constants.oneDayMs * 7
 constants.twoWeeksMs = constants.oneWeekMs * 2
 constants.oneYearMs = 31536000 * 1000
 
+-- EPOCHS
+constants.defaultEpochLength = constants.oneDayMs
+
 -- GAR
 constants.DEFAULT_UNDERNAME_COUNT = 10
 constants.DEADLINE_DURATION_MS = constants.oneHourMs
@@ -17,7 +20,7 @@ constants.MIN_EXPEDITED_WITHDRAWAL_PENALTY_RATE = 0.10 -- the minimum penalty ra
 constants.MAX_EXPEDITED_WITHDRAWAL_PENALTY_RATE = 0.50 -- the maximum penalty rate for an expedited withdrawal (50% of the amount being withdrawn)
 constants.mIOPerIO = 1000000
 constants.minimumWithdrawalAmount = constants.mIOPerIO -- the minimum amount that can be withdrawn from the GAR
-constants.redelegationFeeResetIntervalMs = constants.oneWeekMs -- 7 epochs
+constants.redelegationFeeResetIntervalMs = constants.defaultEpochLength * 7 -- 7 epochs
 constants.maxDelegateRewardShareRatio = 95 -- 95% of rewards can be shared with delegates
 
 -- ARNS
@@ -37,9 +40,9 @@ constants.ARNS_NAME_DOES_NOT_EXIST_MESSAGE = "Name not found in the ArNS Registr
 constants.UNDERNAME_LEASE_FEE_PERCENTAGE = 0.001
 constants.UNDERNAME_PERMABUY_FEE_PERCENTAGE = 0.005
 constants.PRIMARY_NAME_REQUEST_COST = 10000000 -- 10 IO
-constants.gracePeriodMs = constants.twoWeeksMs
+constants.gracePeriodMs = constants.defaultEpochLength * 14 -- 14 epochs
 constants.maxLeaseLengthYears = 5
-constants.returnedNamePeriod = constants.twoWeeksMs
+constants.returnedNamePeriod = constants.defaultEpochLength * 14 -- 14 epochs
 constants.returnedNameMaxMultiplier = 50 -- Freshly returned names will have a multiplier of 50x
 
 constants.ARNS_DISCOUNT_PERCENTAGE = 0.2
