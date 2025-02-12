cd5fd697a4af1f2ed5c14c5f969fa9f50adc74af
dtfiedler <dylan@ar.io>
2025-02-12 13:56:29 -0600

    chore(scripts): create mainnet scrips for extracting state

---
+++ mainnet/create_arns_records_csv.mjs
@@ -0,0 +1,86 @@
+import { AOProcess, ARIO, ARIO_TESTNET_PROCESS_ID } from '@ar.io/sdk';
+import { connect } from '@permaweb/aoconnect';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import fs from 'fs';
+import path from 'path';
+
+const argv = yargs(hideBin(process.argv))
+  .option('processId', {
+    alias: 'p',
+    type: 'string',
+    description: 'Process ID for the gateway registration',
+    default: ARIO_TESTNET_PROCESS_ID,
+  })
+  .option('dryRun', {
+    alias: 'd',
+    type: 'boolean',
+    description: 'Run without making any changes',
+    default: false,
+  })
+  .option('output', {
+    alias: 'o',
+    type: 'string',
+    description: 'Output CSV file path',
+    default: './files/arns_records.csv',
+  })
+  .help()
+  .parseSync();
+
+const processId = argv.processId;
+const dryRun = argv.dryRun;
+const output = argv.output;
+
+console.log(
+  'Pulling records from process',
+  processId,
+  'and writing to',
+  output,
+);
+
+const ario = ARIO.init({
+  process: new AOProcess({
+    processId,
+    ao: connect({
+      CU_URL: 'https://cu.ardrive.io',
+    }),
+  }),
+});
+
+const { items: records } = await ario.getArNSRecords({
+  limit: 5000,
+});
+
+const activeRecords = records.filter(
+  (record) => record.type === 'permabuy' || record.endTimestamp > Date.now(),
+);
+
+// overwrite the file if it exists
+fs.writeFileSync(path.join(process.cwd(), output), '');
+
+// write the header
+fs.appendFileSync(
+  output,
+  'name,processId,type,startTimestamp,endTimestamp,purchasePrice\n',
+);
+
+for (const record of activeRecords) {
+  const { name, processId, type, startTimestamp, endTimestamp, purchasePrice } =
+    record;
+
+  const csvRow = [
+    name,
+    processId,
+    type,
+    startTimestamp,
+    endTimestamp,
+    purchasePrice,
+  ].join(',');
+  if (dryRun) {
+    console.log(csvRow);
+  } else {
+    fs.appendFileSync(output, `${csvRow}\n`);
+  }
+}
+
+console.log('Done');
---
+++ mainnet/create_liquid_balances_csv.mjs
@@ -0,0 +1,144 @@
+import { AOProcess, ARIO, ARIO_TESTNET_PROCESS_ID } from '@ar.io/sdk';
+import { connect } from '@permaweb/aoconnect';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+const argv = yargs(hideBin(process.argv))
+  .option('processId', {
+    alias: 'p',
+    type: 'string',
+    description: 'Process ID for the gateway registration',
+    default: '8Y3BJ6YHtp0uiaM0KSyCdTqLYVJvGLZZI5QjPPskL7I', // the airdrop process ID
+  })
+  .option('dryRun', {
+    alias: 'd',
+    type: 'boolean',
+    description: 'Run without making any changes',
+    default: false,
+  })
+  .option('output', {
+    alias: 'o',
+    type: 'string',
+    description: 'Output CSV file path',
+    default: './files/liquid_balances.csv',
+  })
+  .option('output-delegates', {
+    alias: 'l',
+    type: 'string',
+    description: 'Output CSV file path for assigned delegates',
+    default: './files/assigned_delegates.csv',
+  })
+  .option('gatewaysFile', {
+    alias: 'g',
+    type: 'string',
+    description: 'Gateways CSV file path',
+    default: './files/gateways.csv',
+  })
+  .option('staked-multiplier', {
+    alias: 'm',
+    type: 'number',
+    description: 'The multiplier for staked wallets',
+    default: 1.25, // 25% for staked wallets
+  })
+  .help()
+  .parseSync();
+
+const processId = argv.processId;
+const dryRun = argv.dryRun;
+const output = argv.output;
+const gatewaysFile = argv.gatewaysFile;
+const stakedMultiplier = argv.stakedMultiplier;
+const outputDelegates = argv.outputDelegates;
+console.log(
+  'Pulling liquid balances from AIRDROP process',
+  processId,
+  'and writing to',
+  output,
+);
+
+const airdrop = new AOProcess({
+  processId,
+  ao: connect({
+    CU_URL: 'https://cu.ardrive.io',
+  }),
+});
+
+const { items: registrants } = await airdrop.read({
+  tags: [
+    {
+      name: 'Action',
+      value: 'Get-Registrations',
+    },
+    {
+      name: 'Page-Size',
+      value: '15000',
+    },
+  ],
+});
+
+// overwrite the file if it exists
+fs.writeFileSync(path.join(process.cwd(), output), '');
+
+// write the header
+fs.appendFileSync(output, 'address,mARIOQty\n');
+
+const liquidRegistrants = registrants.filter((r) => r.type === 'wallet');
+
+// assign the liquid balances
+for (const registrant of liquidRegistrants) {
+  const { address, mARIOBaseQty } = registrant;
+
+  const csvRow = [address, mARIOBaseQty].join(',');
+  if (dryRun) {
+    console.log(csvRow);
+  } else {
+    fs.appendFileSync(output, `${csvRow}\n`);
+  }
+}
+
+// assign the delegates
+fs.writeFileSync(path.join(process.cwd(), outputDelegates), '');
+
+// write the header
+fs.appendFileSync(
+  outputDelegates,
+  'gatewayAddress,delegateAddress,delegateStake\n',
+);
+
+const gatewayAddresses = fs
+  .readFileSync(gatewaysFile, 'utf8')
+  .split('\n')
+  .slice(1) // Skip header row
+  .filter(Boolean) // Remove empty lines
+  .map((line) => line.split(',')[1]); // Get just the address column
+const stakedRegistrants = registrants.filter((r) => r.type === 'stake');
+
+console.log(
+  `Assigning ${stakedRegistrants.length} staked registrations to ${gatewayAddresses.length} gateways`,
+);
+
+for (const registrant of stakedRegistrants) {
+  const { address: delegateAddress, mARIOBaseQty } = registrant;
+
+  // compute the assigned gateway based on the hash of the address
+  const delegateHash = crypto
+    .createHash('sha256')
+    .update(delegateAddress)
+    .digest('hex');
+  const truncatedHash = delegateHash.substring(0, 8);
+  const gatewayAddress =
+    gatewayAddresses[parseInt(truncatedHash, 16) % gatewayAddresses.length];
+
+  // assign the delegate
+  const delegateStake = mARIOBaseQty * stakedMultiplier;
+  const csvRow = [gatewayAddress, delegateAddress, delegateStake].join(',');
+  if (dryRun) {
+    console.log(csvRow);
+  } else {
+    fs.appendFileSync(outputDelegates, `${csvRow}\n`);
+  }
+}
+
+console.log('Done');
