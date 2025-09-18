ac230d22273d0d8a635a3135acffc26db8e1616c
atticusofsparta <atticusofsparta@protonmail.com>
2025-09-18 11:00:34 -0600

    fix(patch): fix file name and add notes

--- patches/2025-09-18-undername-for-name.lua
+++ patches/2025-09-18-undername-for-name.lua
@@ -1,3 +1,10 @@
+--[[
+	Fixes the undernameForName function to handle names with dashes.
+
+	Reviewers: Dylan, Ariel, Atticus
+]]
+--
+-- utils.lua - override undernameForName function
 _G.package.loaded[".src.utils"].undernameForName = function(name)
 	local reversedName = name:reverse()
 
